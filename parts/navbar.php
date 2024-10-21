c238099c83b138ccc0c896affc4ab6b22d24fa4f
Satria Nugraha <satrianugraha2007@gmail.com>
2024-10-21 10:26:10 +0700

    add flightphp

--- parts/navbar.php
+++ parts/navbar.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-expand-sm navbar-light bg-light sticky-top">
     <div class="container">
-        <a class="navbar-brand text-black" href="../index.php">Reliquid Project</a>
+        <a class="navbar-brand text-black" href="../old_index.php">Reliquid Project</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon text-white"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link text-black" href="../index.php">Home</a>
+                    <a class="nav-link text-black" href="../old_index.php">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link text-black" href="../news.php">News</a>
